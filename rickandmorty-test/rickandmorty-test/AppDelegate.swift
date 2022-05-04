f2e90b10ec3bdbf9049eab17b3f34c80ff5ac89b
molexey <user@Mac-mini.local>
2022-05-04 14:39:12 +0300

    Initial project without storyboard

---
+++ rickandmorty-test/rickandmorty-test/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  rickandmorty-test
+//
+//  Created by molexey on 04.05.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+}
